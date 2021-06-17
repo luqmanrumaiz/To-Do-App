af295b8478d98e58a21f754310bcc580bbc7d188
Luqman Rumaiz <61153637+luqmanrumaiz@users.noreply.github.com>
2021-06-17 21:54:55 +0530

    Added Floating Button

--- to-do-app/src/App.js
+++ to-do-app/src/App.js
@@ -1,16 +1,33 @@
 import './App.css';
 import {Box, Center, ChakraProvider} from "@chakra-ui/react"
+import AddIcon from '@material-ui/icons/Add';
+import {useState} from "react";
 
 function App() {
-  return (
-      <ChakraProvider>
-        <Box>
-            <Center bg="teal" h="120px" color="white">
-                <h1>To-Do App</h1>
-            </Center>
-        </Box>
-      </ChakraProvider>
-  );
+
+    const [tasks, setTasks] = useState([]);
+
+    
+
+    return (
+        <ChakraProvider>
+            <Box>
+                <Center bg="teal" h="120px" color="white">
+                    <h1>To-Do App</h1>
+                </Center>
+            </Box>
+
+            {tasks == null ? (
+                <h1>No Tasks</h1>
+            ) : (
+                <h1>Tasks</h1>
+            )}
+
+            <a href="#" className="float">
+                <AddIcon className="my-float"/>
+            </a>
+        </ChakraProvider>
+    );
 }
 
 export default App;
